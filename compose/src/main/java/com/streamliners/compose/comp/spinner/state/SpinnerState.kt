a51dafb46219be15ffd9764b65cc049a14e2f771
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-02-26 11:15:30 +0530

    OutlinedSpinner State - update TIL value based on prefill

--- compose/src/main/java/com/streamliners/compose/comp/spinner/state/SpinnerState.kt
+++ compose/src/main/java/com/streamliners/compose/comp/spinner/state/SpinnerState.kt
@@ -2,12 +2,20 @@ package com.streamliners.compose.comp.spinner.state
 
 import androidx.compose.runtime.MutableState
 import com.streamliners.compose.comp.textInput.state.TextInputState
+import com.streamliners.compose.comp.textInput.state.update
 
 class SpinnerState<T>(
     val selection: MutableState<T?>,
     val textInputState: MutableState<TextInputState>,
     val labelExtractor: (T) -> String
 ) {
+    init {
+        // Update TIS value based on prefill
+        selection.value?.let(labelExtractor)?.let {
+            textInputState.update(it)
+        }
+    }
+
     companion object
 
     fun ifSelected(
