ab02015376bd727abe3ce8c41f06c14409243408
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-03-08 03:49:33 +0530

    make utils as a plain java lib

--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -1,36 +1,11 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.jvm")
     id("maven-publish")
 }
 
-android {
-    namespace = "com.streamliners.utils"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
@@ -39,8 +14,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 publishing {
@@ -50,9 +23,7 @@ publishing {
             artifactId = "utils"
             version = "1.0"
 
-            afterEvaluate {
-                from(components["release"])
-            }
+            from(components["java"])
         }
     }
 }
