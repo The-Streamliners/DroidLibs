69c54f816639b55837fec7e515d6035733415dbb
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-03-08 03:55:33 +0530

    utils: remove unnecessary dependencies

--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -9,10 +9,6 @@ java {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
 }
 
