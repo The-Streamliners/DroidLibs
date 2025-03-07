20b0d6acccfe3df6cb4773cbaeb380a7416b876e
Lavish Swarnkar <lavishswarnkar@gmail.com>
2025-03-07 19:04:43 +0530

    fixes #10 : Update AyncImage composable to expose placeholder

--- compose-android/src/main/java/com/streamliners/compose/android/comp/AsyncImage.kt
+++ compose-android/src/main/java/com/streamliners/compose/android/comp/AsyncImage.kt
@@ -1,5 +1,6 @@
 package com.streamliners.compose.android.comp
 
+import android.graphics.drawable.Drawable
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
@@ -16,12 +17,23 @@ fun AsyncImage(
     data: String,
     contentScale: ContentScale = ContentScale.Fit,
     circleCrop: Boolean = false,
+    placeHolderResId: Int? = null,
+    placeHolderDrawable: Drawable? = null,
     onClick: (() -> Unit)? = null
 ) {
     AsyncImage(
         model = ImageRequest.Builder(LocalContext.current)
             .data(data)
             .crossfade(true)
+            .run {
+                if (placeHolderResId != null) {
+                    placeholder(placeHolderResId)
+                } else if (placeHolderDrawable != null) {
+                    placeholder(placeHolderDrawable)
+                } else {
+                    this
+                }
+            }
             .build(),
         contentDescription = "",
         contentScale = contentScale,
